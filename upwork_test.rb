cf0fc244a7078a2e953a4491e2dfee210dad550f
Andriy Samoxin <australi@inbox.ru>
2019-05-09 17:31:31 +0300

    update upwork_test

--- upwork_test.rb
+++ upwork_test.rb
@@ -8,14 +8,19 @@ results.each do |row|
   before_parenth.downcase! if before_parenth
   names = row['candidate_office_name'].sub(/(\w+\s*\w*)(,\s*)((\w+\s*){1,})/, "#{before_parenth} (\\3)").split /\//
   become_first = names.pop if names.size > 1
-  names.map!(&:downcase) unless names.detect {|name| name.include?('(')}
+  names.map(&:downcase!) unless names.detect { |name| name.include?('(') }
+  become_first.strip! if become_first
   names.unshift(become_first).compact!
-  names.map! { |name| name.gsub(/hwy/i, 'Highway') }
-  names.map! { |name| name.gsub(/twp/i, 'Township') }
-  names.map! { |name| name.gsub('.', '') }
-  names.map! { |name| name.gsub("'", '') }
+  [["'", '`'], ['.', ''], [/twp/i, 'Township'], [/hwy/i, 'Highway']].each do |old_val, new_val|
+    names.map! { |name| name.gsub(old_val, new_val) }
+  end
   names.insert(-2, 'and') if names.size > 2
-  clean_name = "clean_name = '#{names.join(' ')}'"
-  sentence = "sentence = 'The candidate is running for the #{names.join(' ')} office'"
+  names.each_slice(2) do |f_name, l_name|
+    first_word = l_name[/\w+/]
+    l_name.sub!(/\w+\s/, '') if first_word && f_name[/\w+$/] == first_word.capitalize
+  end if names.size > 1
+  joined_names = names.join(' ')
+  clean_name = "clean_name = '#{joined_names}'"
+  sentence = "sentence = 'The candidate is running for the #{joined_names} office'"
   client.query("update #{table} set #{clean_name}, #{sentence} where id='#{row['id']}'")
 end
